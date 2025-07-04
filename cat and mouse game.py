7e05d0c200ddcfe43f201304d581137964087530
iamajetlearner <hcmo2023@gmail.com>
2025-07-04 17:39:53 +0200

    Update cat and mouse game.py

--- cat and mouse game.py
+++ cat and mouse game.py
@@ -1,4 +1,7 @@
 import pgzrun
+import random
+score=0
+game=True
 WIDTH=500
 HEIGHT=500
 camous=Actor("cat")
@@ -9,9 +12,14 @@ mousat.y=200
 mousat.x=200
 def draw():
     screen.blit("grassfield",(0,0))
-    camous.draw()
-    mousat.draw()
+    if game==True:
+        camous.draw()
+        mousat.draw()
+        screen.draw.text(str(score),(50,50))
+    else:
+        screen.draw.text("game-over",(50,20))
 def update():
+    global score
     if keyboard.w:
         camous.y-=10
     elif keyboard.s:
@@ -20,4 +28,12 @@ def update():
         camous.x-=10
     elif keyboard.d:
         camous.x+=10
+    if camous.colliderect(mousat):
+        mousat.x =random.randint(100,400)
+        mousat.y =random.randint(100,400)
+        score+=1
+def tomer():
+    global game
+    game=False
+clock.schedule(tomer,10)
 pgzrun.go()
